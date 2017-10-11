8018cedc9a812539d5daaabd6942d1f945122e97
ClockworkKettle <timothyjmcnamara@Gmail.com>
2017-10-11 18:10:18 +0100

    Loop Bug Fix -- Added val1 reset inside outer loop

--- BarcodeGenerator/Avery5161pdf.cs
+++ BarcodeGenerator/Avery5161pdf.cs
@@ -19,26 +19,32 @@ namespace BarcodeGenerator
             XFont barcodeFont = new XFont("Verdana", 14, XFontStyle.Regular);
             gfx.DrawString("Barcodes", titleFont, XBrushes.Black, new XRect(0, 0, page.Width, page.Height), XStringFormats.TopCenter);
 
+            //for (int i = 0; i < Int32.Parse(barcodeCounted); i++)
+            //{
+            //    gfx.DrawString("I am a barcode", barcodeFont, XBrushes.Black, new XRect(0, 0, page.Width, page.Height), XStringFormats.Center);
+            //}
+
             //US Letter Page Dimensions: 612 * 792
             string text = "I am a barcode";
             XRect rect;
             int barcodeCount = int.Parse(rangeCount);
-			int barcodeCountDivTen = (barcodeCount/10 )+1;
-			int val1=12;
-            int val2=36;
+            int barcodeCountDivTen = (barcodeCount / 10) + 1;
+            int val1 = 12;
+            int val2 = 36;
 
-			for(int j=0; j<barcodeCountDivTen; j++)
-			{
-				for (int i =0; i<barcodeCount; i++)
-				{
-					rect = new XRect(12, val2, 288, 72);
+            for (int j = 0; j < barcodeCountDivTen; j++)
+            {
+                for (int i = 0; ((i < barcodeCount) && (i < 10)); i++)
+                {
+                    rect = new XRect(val1, val2, 288, 72);
                     gfx.DrawRectangle(XBrushes.White, rect);
                     tf.Alignment = XParagraphAlignment.Left;
                     tf.DrawString(text, barcodeFont, XBrushes.Black, rect, XStringFormats.TopLeft);
-					val2+=72;
-				}
-				val1+=301;
-				barcodeCount-=10;
+                    val2 += 72;
+                }
+                val2 = 36;
+                val1 += 301;
+                barcodeCount -= 10;
             }
             const string filename = "Barcodes.pdf";
             document.Save(filename);
